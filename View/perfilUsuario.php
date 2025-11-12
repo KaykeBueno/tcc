3e0e1bd7d5437b7bd348a0ee560cd2cf4c0f48f1
LFelipeRamos <142407961+LFelipeRamos@users.noreply.github.com>
2025-11-12 14:44:08 -0300

    adiciona o tedirecionamento do sac para whatsapp

--- View/perfilUsuario.php
+++ View/perfilUsuario.php
@@ -2,7 +2,7 @@
 
         <main id="content">
             <div class="dashboard-grid">
-                <a href="#" class="dashboard-card">
+                <a href="cardEmpresa.php" class="dashboard-card">
                     <img src="../imagens/ProfessorIcone.png" alt="Ícone Professores">
                     <h5>Professores</h5>
                     <p>Visualize seus professores e acesse informações sobre seus responsáveis pelos treinos.</p>
@@ -14,11 +14,11 @@
                     <p>Acompanhe seus treinos e veja os exercícios recomendados para você.</p>
                 </a>
 
-                <a href="#" class="dashboard-card">
+               <!-- <a href="#" class="dashboard-card">
                     <img src="../imagens/quizIcone.png" alt="Ícone Quiz">
                     <h5>Quiz</h5>
                     <p>Responda perguntas e receba indicações de esportes ideais para o seu perfil e estilo de vida.</p>
-                </a>
+                </a> --> 
 
                 <a href="#" class="dashboard-card">
                     <img src="../imagens/chatIcone.png" alt="Ícone Chat">
@@ -32,7 +32,7 @@
                     <p>Acompanhe sua presença nas aulas e veja seu histórico de participação.</p>
                 </a>
 
-                <a href="#" class="dashboard-card">
+                <a href="https://wa.me/5543988223904" class="dashboard-card">
                     <img src="../imagens/SacIcone.png" alt="Ícone SAC">
                     <h5>SAC</h5>
                     <p>Entre em contato com o suporte para tirar dúvidas ou relatar problemas.</p>
