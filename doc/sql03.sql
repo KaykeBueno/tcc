c73406d07c52b23353851ade7c9529791132883c
LFelipeRamos <142407961+LFelipeRamos@users.noreply.github.com>
2025-11-12 15:08:49 -0300

    adiciona novo script SQL para criação do esquema e tabelas do banco de dados juntamente com o DER atualizado

---
+++ doc/sql03.sql
@@ -0,0 +1,182 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema tcc
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema tcc
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `tcc` DEFAULT CHARACTER SET utf8 ;
+USE `tcc` ;
+
+-- -----------------------------------------------------
+-- Table `tcc`.`admin`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `tcc`.`admin` (
+  `idAdmin` INT(11) NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(100) NOT NULL,
+  `senha` VARCHAR(255) NOT NULL,
+  `email` VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`idAdmin`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `tcc`.`empresa`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `tcc`.`empresa` (
+  `id_empresa` INT(11) NOT NULL AUTO_INCREMENT,
+  `cnpj` VARCHAR(18) NOT NULL,
+  `senha` VARCHAR(255) NOT NULL,
+  `nomeFantasia` VARCHAR(100) NOT NULL,
+  `telefone` VARCHAR(20) NOT NULL,
+  `email` VARCHAR(100) NOT NULL,
+  `atividadeEconomica` VARCHAR(100) NOT NULL,
+  `porteEmpresarial` VARCHAR(50) NOT NULL,
+  `status` BIT(1) NOT NULL DEFAULT b'1',
+  PRIMARY KEY (`id_empresa`),
+  UNIQUE INDEX `cnpj_UNIQUE` (`cnpj` ASC))
+ENGINE = InnoDB
+AUTO_INCREMENT = 6
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `tcc`.`usuario`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `tcc`.`usuario` (
+  `id_usuario` INT(11) NOT NULL AUTO_INCREMENT,
+  `cpf` VARCHAR(14) NOT NULL,
+  `nome` VARCHAR(100) NOT NULL,
+  `senha` VARCHAR(255) NOT NULL,
+  `telefone` VARCHAR(20) NOT NULL,
+  `dataNascimento` DATE NOT NULL,
+  `email` VARCHAR(100) NOT NULL,
+  `sexo` VARCHAR(20) NOT NULL,
+  PRIMARY KEY (`id_usuario`),
+  UNIQUE INDEX `cpf_UNIQUE` (`cpf` ASC))
+ENGINE = InnoDB
+AUTO_INCREMENT = 2
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `tcc`.`avaliações`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `tcc`.`avaliações` (
+  `idAvaliações` INT(11) NOT NULL AUTO_INCREMENT,
+  `nota` INT(11) NOT NULL,
+  `comentario` VARCHAR(255) NULL DEFAULT NULL,
+  `dataAvaliacao` DATE NOT NULL,
+  `Usuario_id_usuario` INT(11) NOT NULL,
+  `Empresa_id_empresa` INT(11) NOT NULL,
+  PRIMARY KEY (`idAvaliações`),
+  INDEX `fk_Avaliações_Usuario_idx` (`Usuario_id_usuario` ASC),
+  INDEX `fk_Avaliações_Empresa1_idx` (`Empresa_id_empresa` ASC),
+  CONSTRAINT `fk_Avaliações_Empresa1`
+    FOREIGN KEY (`Empresa_id_empresa`)
+    REFERENCES `tcc`.`empresa` (`id_empresa`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Avaliações_Usuario`
+    FOREIGN KEY (`Usuario_id_usuario`)
+    REFERENCES `tcc`.`usuario` (`id_usuario`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `tcc`.`logacesso`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `tcc`.`logacesso` (
+  `idlogAcesso` INT(11) NOT NULL AUTO_INCREMENT,
+  `dataAcesso` DATETIME NOT NULL,
+  `Usuario_id_usuario` INT(11) NOT NULL,
+  `Empresa_id_empresa` INT(11) NOT NULL,
+  PRIMARY KEY (`idlogAcesso`),
+  INDEX `fk_logAcesso_Usuario1_idx` (`Usuario_id_usuario` ASC),
+  INDEX `fk_logAcesso_Empresa1_idx` (`Empresa_id_empresa` ASC),
+  CONSTRAINT `fk_logAcesso_Empresa1`
+    FOREIGN KEY (`Empresa_id_empresa`)
+    REFERENCES `tcc`.`empresa` (`id_empresa`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_logAcesso_Usuario1`
+    FOREIGN KEY (`Usuario_id_usuario`)
+    REFERENCES `tcc`.`usuario` (`id_usuario`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `tcc`.`treino`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `tcc`.`treino` (
+  `idTreino` INT(11) NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(100) NOT NULL,
+  `descricao` TEXT NOT NULL,
+  PRIMARY KEY (`idTreino`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `tcc`.`ofertaTreino`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `tcc`.`ofertaTreino` (
+  `idOferta` VARCHAR(45) NOT NULL,
+  `empresa_id_empresa` INT(11) NOT NULL,
+  `treino_idTreino` INT(11) NOT NULL,
+  INDEX `fk_empresa_has_treino_treino1_idx` (`treino_idTreino` ASC),
+  INDEX `fk_empresa_has_treino_empresa1_idx` (`empresa_id_empresa` ASC),
+  PRIMARY KEY (`idOferta`),
+  CONSTRAINT `fk_empresa_has_treino_empresa1`
+    FOREIGN KEY (`empresa_id_empresa`)
+    REFERENCES `tcc`.`empresa` (`id_empresa`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_empresa_has_treino_treino1`
+    FOREIGN KEY (`treino_idTreino`)
+    REFERENCES `tcc`.`treino` (`idTreino`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `tcc`.`usuarioTreino`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `tcc`.`usuarioTreino` (
+  `usuario_id_usuario` INT(11) NOT NULL,
+  `ofertaTreino_idOferta` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`usuario_id_usuario`, `ofertaTreino_idOferta`),
+  INDEX `fk_usuario_has_ofertaTreino_ofertaTreino1_idx` (`ofertaTreino_idOferta` ASC),
+  INDEX `fk_usuario_has_ofertaTreino_usuario1_idx` (`usuario_id_usuario` ASC),
+  CONSTRAINT `fk_usuario_has_ofertaTreino_usuario1`
+    FOREIGN KEY (`usuario_id_usuario`)
+    REFERENCES `tcc`.`usuario` (`id_usuario`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_usuario_has_ofertaTreino_ofertaTreino1`
+    FOREIGN KEY (`ofertaTreino_idOferta`)
+    REFERENCES `tcc`.`ofertaTreino` (`idOferta`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
