20df38fe214725329119255bf5b9f8f8197c440f
LFelipeRamos <142407961+LFelipeRamos@users.noreply.github.com>
2025-11-13 22:49:49 -0300

    adiciona a classe Portifolio com implementação de JsonSerializable e métodos de acesso

---
+++ entities/Portifolio.php
@@ -0,0 +1,56 @@
+<?php
+
+class Portifolio implements JsonSerializable
+{
+    private $idPortifolio;
+    private $empresa_id_empresa;
+    private $treino_idTreino;
+
+    public function jsonSerialize(): mixed
+    {
+        return [
+            'idPortifolio' => $this->idPortifolio,
+            'empresa_id_empresa' => $this->empresa_id_empresa,
+            'treino_idTreino' => $this->treino_idTreino,
+        ];
+    }
+
+    function __construct($idPortifolio = "", $empresa_id_empresa = "", $treino_idTreino = "")
+    {
+        $this->idPortifolio = $idPortifolio;
+        $this->empresa_id_empresa = $empresa_id_empresa;
+        $this->treino_idTreino = $treino_idTreino;
+    }
+
+    // Getters / Setters
+    public function getIdPortifolio()
+    {
+        return $this->idPortifolio;
+    }
+
+    public function setIdPortifolio($idPortifolio)
+    {
+        $this->idPortifolio = $idPortifolio;
+    }
+
+    public function getEmpresa_id_empresa()
+    {
+        return $this->empresa_id_empresa;
+    }
+
+    public function setEmpresa_id_empresa($empresa_id_empresa)
+    {
+        $this->empresa_id_empresa = $empresa_id_empresa;
+    }
+
+    public function getTreino_idTreino()
+    {
+        return $this->treino_idTreino;
+    }
+
+    public function setTreino_idTreino($treino_idTreino)
+    {
+        $this->treino_idTreino = $treino_idTreino;
+    }
+}
+?>
